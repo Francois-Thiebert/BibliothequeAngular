9d5f805c2e5ae7bf955f027efebfcbe0af924690
Alix Herbelot <alix.herbelot@gmail.com>
2023-04-28 09:33:28 +0200

    Modif du bouton bonjour pour les admins

--- projetBibliothequeAngular/src/app/components/home/home.component.ts
+++ projetBibliothequeAngular/src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Adherent } from 'src/app/model/adherent';
 import { Livre } from 'src/app/model/livre';
+import { Role } from 'src/app/model/role';
+import { Utilisateur } from 'src/app/model/utilisateur';
 import { EmpruntService } from 'src/app/services/emprunt.service';
 import { LivreService } from 'src/app/services/livre.service';
 
@@ -31,5 +33,30 @@ export class HomeComponent implements OnInit{
       this.livres = livres;
     });
   }
+
+  get logged(): boolean {
+    return sessionStorage.getItem('token') ? true : false;
+  }
+
+  get admin(): boolean {
+    if (sessionStorage.getItem('utilisateur')) {
+      let utilisateur: Utilisateur  = JSON.parse(
+        sessionStorage.getItem('utilisateur')!
+      ) as Utilisateur;
+      return utilisateur.role == Role.ROLE_ADMIN;
+    }
+    return false;
+  }
+
+  get adherent(): boolean {
+    if (sessionStorage.getItem('utilisateur')) {
+      let utilisateur: Utilisateur  = JSON.parse(
+        sessionStorage.getItem('utilisateur')!
+      ) as Utilisateur;
+      return utilisateur.role == Role.ROLE_ADHERENT;
+    }
+    return false;
+  }
+
 }
 
